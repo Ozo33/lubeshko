6cb994a7997411df1ed56996cc8faf82bb11062a
Ozo33 <161723315+Ozo33@users.noreply.github.com>
2025-12-02 21:26:02 +0200

    Add files via upload

---
+++ kursova_Lubeshko/Scripts/Script.js
@@ -0,0 +1,42 @@
+const scrollTopBtn = document.getElementById('scrollTopBtn');
+if (scrollTopBtn) {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 700) {
+            scrollTopBtn.classList.add('show');
+            scrollTopBtn.classList.remove('hide');
+        } else {
+            scrollTopBtn.classList.add('hide');
+            setTimeout(() => scrollTopBtn.classList.remove('show'), 300);
+        }
+    });
+
+    scrollTopBtn.addEventListener('click', () => {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+}
+
+const openModal = document.getElementById("openModal");
+const modalOverlay = document.getElementById("modalOverlay");
+const closeModal = document.getElementById("closeModal");
+
+openModal.addEventListener("click", () => {
+    modalOverlay.style.display = "flex";
+});
+
+closeModal.addEventListener("click", () => {
+    modalOverlay.style.display = "none";
+});
+
+// Закриття по кліку на оверлей
+modalOverlay.addEventListener("click", (e) => {
+    if (e.target === modalOverlay) {
+        modalOverlay.style.display = "none";
+    }
+});
+
+// Закриття по ESC
+document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+        modalOverlay.style.display = "none";
+    }
+});
